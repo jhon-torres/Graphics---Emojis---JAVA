c3d7375d62ab810f717a1d2c4f7bc172fac2484c
Feliandres <85601974+Feliandres@users.noreply.github.com>
2022-07-16 17:48:50 -0500

    Agregando emojis

--- src/Emojis.java
+++ src/Emojis.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Arc2D;
 
 public class Emojis extends JPanel{
     private int opcion;
@@ -110,9 +111,83 @@ public class Emojis extends JPanel{
                 g.setColor( Color.BLACK );
                 g.fillOval( 50, 130, 120, 60 );
 
+
                 g.setColor( Color.YELLOW );
                 g.fillRect( 50, 160, 120, 30 );
                 g.fillOval( 50, 140, 120, 40 );
+                break;
+
+            case 6:
+                /* EMOJI GUIÑANDO EL OJO */
+
+                // CARA
+                g.setColor( Color.YELLOW );
+                g.fillOval( 10, 10, 200, 200 );
+                // OJOS
+                g.setColor( Color.BLACK );
+                g.fillOval( 65, 65, 30, 40 );
+                g2d.draw(new Arc2D.Double(135,85,35,15,0,180,Arc2D.CHORD));
+                g2d.fill(new Arc2D.Double(135,85,35,15,0,180,Arc2D.CHORD));
+                // CEJAS
+                g2d.fill(new Arc2D.Double(55,45,45,10,40,200,Arc2D.CHORD));
+                g2d.fill(new Arc2D.Double(135,65,35,15,-20,160,Arc2D.CHORD));
+                // BOCA
+                g.fillOval( 75, 110, 80, 70 );
+                g.setColor( Color.YELLOW );
+                g.fillRect( 75, 110, 80, 35 );
+                g.fillOval( 75, 120, 80, 45 );
+                break;
+
+            case 7:
+                /* EMOJI ENOJADO */
+
+                // CARA
+                g.setColor( Color.YELLOW );
+                g.fillOval( 10, 10, 200, 200 );
+                // OJOS
+                g.setColor( Color.BLACK );
+                g.fillOval( 75, 100, 20, 30 );
+                g.fillOval( 125, 100, 20, 30 );
+                // CEJAS
+                g2d.fill(new Arc2D.Double(40,90,55,10,-50,170,Arc2D.CHORD));
+                g2d.fill(new Arc2D.Double(125,90,55,10,50,190,Arc2D.CHORD));
+                // BOCA
+                g.fillOval( 80, 150, 60, 50 );
+                g.setColor( Color.YELLOW );
+                g.fillRect( 80, 170, 60, 25 );
+                g.fillOval( 80, 160, 60, 45 );
+                break;
+
+            case 8:
+                /* EMOJI FELIZ AL REVES */
+
+                // CARA
+                g.setColor( Color.YELLOW );
+                g.fillOval( 10, 10, 200, 200 );
+                // OJOS
+                g.setColor( Color.BLACK );
+                g.fillOval( 75, 125, 20, 30 );
+                g.fillOval( 135, 125, 20, 30 );
+                // BOCA
+                g.fillOval( 65, 50, 100, 50 );
+                g.setColor( Color.YELLOW );
+                g.fillRect( 65, 70, 100, 25 );
+                g.fillOval( 65, 60, 100, 45 );
+                break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
