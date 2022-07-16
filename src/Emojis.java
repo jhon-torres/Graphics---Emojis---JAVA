3d82b232ab15c3fb0ada6a6579cacee4d3b27549
Jhon <jhon-3316@hotmail.com>
2022-07-16 13:11:54 -0500

    Creacion menu hasta la opcion emoji 5 funcional

---
+++ src/Emojis.java
@@ -0,0 +1,118 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Emojis extends JPanel{
+    private int opcion;
+
+    public Emojis(int opcion) {
+        this.opcion = opcion;
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        /* uso para el grosor de lineas, usado en opcion 4 */
+        Graphics2D g2d = (Graphics2D)g;
+        BasicStroke grosor = new BasicStroke(5);
+        g2d.setStroke(grosor);
+
+        switch (opcion){
+
+            case 1:
+                //EMOJI FELIZ
+
+                g.setColor( Color.YELLOW );
+                g.fillOval( 10, 10, 200, 200 );
+
+                g.setColor( Color.BLACK );
+                g.fillOval( 55, 65, 30, 30 );
+                g.fillOval( 135, 65, 30, 30 );
+
+                g.fillOval( 50, 110, 120, 60 );
+
+                g.setColor( Color.YELLOW );
+                g.fillRect( 50, 110, 120, 30 );
+                g.fillOval( 50, 120, 120, 40 );
+                break;
+            case 2:
+                //EMOJI SORPRESA
+
+                g.setColor( Color.YELLOW );
+                g.fillOval( 10, 10, 200, 200 );
+
+                g.setColor( Color.BLACK );
+                g.fillOval( 55, 65, 30, 30 );
+                g.fillOval( 135, 65, 30, 30 );
+
+                g.fillOval( 80, 110, 60, 60 );
+                break;
+            case 3:
+                // CORAZON
+                g.setColor( Color.RED);
+
+                // PUNTOS DE LINES
+                /*
+                (X1, Y1) (X2, Y2)
+                (130, 75) (100, 75)
+                (100, 75) (70, 105)
+                (70, 105) (70, 125)
+                (70, 125) (90, 145)
+                (90, 145) (140, 185)
+                (140, 185) (190, 145)
+                (190, 145) (210, 125)
+                (210, 125) (210, 105)
+                (210, 105) (190, 75)
+                (190, 75) (160, 75)
+                (160, 75) (140, 105)
+                (140, 105) (130, 75)
+                */
+                int[] puntosX = {130, 100, 70, 70, 90, 140, 190, 210, 210, 190, 160, 140};
+                int[] puntosY = {75, 75, 105, 125, 145, 185, 145, 125, 105, 75, 75, 105};
+
+                g.fillPolygon(puntosX, puntosY, 12);
+                break;
+            case 4:
+                //EMOJI SACANDO LENGUA
+
+                g.setColor( Color.YELLOW );
+                g.fillOval( 10, 10, 200, 200 );
+
+                g.setColor( Color.BLACK );
+                g.fillOval( 55, 65, 30, 30 );
+
+
+                g2d.drawLine(135,80, 165, 65);
+                g2d.drawLine(135,80, 165, 95);
+
+                g.fillOval( 50, 110, 120, 60 );
+
+                g.setColor( Color.YELLOW);
+                g.fillRect( 50, 100, 120, 30 );
+                g.setColor( Color.RED);
+                g.fillOval( 85, 140, 30, 50 );
+                g.fillOval( 105, 140, 30, 50 );
+                g.fillOval( 95, 175, 30, 25 );
+                break;
+            case 5:
+                //EMOJI TRISTE
+
+                g.setColor( Color.YELLOW );
+                g.fillOval( 10, 10, 200, 200 );
+
+                g.setColor( Color.BLACK );
+                g.fillOval( 55, 65, 30, 30 );
+                g.fillOval( 135, 65, 30, 30 );
+
+                g.setColor(Color.YELLOW);
+                g.fillOval(55,80,30,40);
+                g.fillOval(135,80,30,40);
+
+                g.setColor( Color.BLACK );
+                g.fillOval( 50, 130, 120, 60 );
+
+                g.setColor( Color.YELLOW );
+                g.fillRect( 50, 160, 120, 30 );
+                g.fillOval( 50, 140, 120, 40 );
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        int opcion=0;
+        /* repeticion del menú hasta que escriba opc 13 */
+        while (opcion != 13){
+            /* MENU DE OPCIONES*/
+            String entrada = JOptionPane.showInputDialog(
+                    """
+                            Escriba 1 para dibujar emoji feliz
+                            Escriba 2 para dibujar emoji sorpresa
+                            Escriba 3 para dibujar emoji de corazón
+                            Escriba 4 para dibujar emoji sacando la lengua
+                            Escriba 5 para dibujar emoji triste
+                            Escriba 13 para salir
+                            """);
+            opcion=Integer.parseInt(entrada);
+
+            if (opcion <13 && opcion>0){
+                Emojis panel =new Emojis (opcion);
+                JFrame aplicacion = new JFrame();
+
+                aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                aplicacion.add(panel);
+                aplicacion.setSize(300,300);
+                aplicacion.setVisible(true);
+                /* DELAY */
+                Thread.sleep(500);
+            }
+            else if (opcion == 13){
+                JOptionPane.showMessageDialog(null, "Gracias por usar el programa!!.");
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "Elige correctamente la opción.");
+            }
+
+        }
+
+        /* cierre del programa completo junto a JFrame */
+        System.exit( 0 );
+
+    }
+}
